b54ec8c8e6ae747599995ab496804acdc3982d5f
Fernando Aguiar Simões <112782166+fsimoes90@users.noreply.github.com>
2023-11-14 14:32:13 -0300

    Add files via upload

---
+++ Alura/Jogo de adivinhacao/jogoAdivinhacao.js
@@ -0,0 +1,23 @@
+var numeroSecreto = 581;
+var chute;
+
+while (true) {
+    chute = prompt('Digite um número entre 1 e 1000\nDigite "Esc" a qualquer momento para sair');
+    if (chute.toLowerCase() === 'esc') {
+        alert('Você saiu do jogo.');
+        break;
+    }
+    if (isNaN(chute) || chute < 1 || chute > 1000) {
+        alert('Por favor, digite um número válido entre 1 e 1000.');
+        continue;
+    }
+    chute = parseInt(chute);
+    if (chute === numeroSecreto) {
+        alert('Parabéns! Você acertou o número secreto: ' + numeroSecreto);
+        break;
+    } else if (chute < numeroSecreto) {
+        alert('Você errou. O número é maior. Tente novamente.');
+    } else {
+        alert('Você errou. O número é menor. Tente novamente.');
+    }
+}
