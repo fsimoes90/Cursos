20f03841c5234871bc4c908cd799a34bf3cabd1d
Fernando Aguiar Simões <112782166+fsimoes90@users.noreply.github.com>
2023-11-14 14:47:35 -0300

    Update jogoAdivinhacao.js

--- Alura/Jogo de adivinhacao/jogoAdivinhacao.js
+++ Alura/Jogo de adivinhacao/jogoAdivinhacao.js
@@ -1,23 +1,27 @@
 var numeroSecreto = parseInt(Math.random() * 1001);
 var chute;
+var tentativas = 0;
 
 while (true) {
     chute = prompt('Digite um número entre 1 e 1000\nDigite "Esc" a qualquer momento para sair');
+    
     if (chute.toLowerCase() === 'esc') {
         alert('Você saiu do jogo.');
         break;
     }
+
     if (isNaN(chute) || chute < 1 || chute > 1000) {
         alert('Por favor, digite um número válido entre 1 e 1000.');
         continue;
     }
+
     chute = parseInt(chute);
+    tentativas++;
+
     if (chute === numeroSecreto) {
-        alert('Parabéns! Você acertou o número secreto: ' + numeroSecreto);
+        alert('Parabéns! Você acertou o número secreto: ' + numeroSecreto + '\nNúmero de tentativas: ' + tentativas);
         break;
-    } else if (chute < numeroSecreto) {
-        alert('Você errou. O número é maior. Tente novamente.');
     } else {
-        alert('Você errou. O número é menor. Tente novamente.');
+        alert('Você errou.\nO número secreto é ' + (chute < numeroSecreto ? 'maior' : 'menor') + ' ' + chute + '. Tente novamente.\nNúmero de tentativas: ' + tentativas);
     }
 }
