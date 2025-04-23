1657c2964ed3f4139cc17cfd8dd5df4a7ce01ce4
Fernando Aguiar Simões <112782166+fsimoes90@users.noreply.github.com>
2025-04-23 14:42:18 -0300

    Delete OneBitCode directory

--- OneBitCode/index.js
+++
@@ -1,7 +0,0 @@
-var elementosDuvida = 
-    document.querySelectorAll(".duvidas")
-elementosDuvida.forEach(function(duvidas){
-    duvidas.addEventListener('click', function(){
-        duvidas.classList.toggle('ativa')
-    })
-})
